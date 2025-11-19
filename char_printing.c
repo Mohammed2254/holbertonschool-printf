2985b13f308a2b7ef3d4d90db29c631eca382c4d
ahotb <ah.r9625@gmail.com>
2025-11-19 11:26:01 +0000

    Format is set betty

--- char_printing.c
+++ char_printing.c
@@ -2,20 +2,17 @@
 #include <unistd.h>
 #include <stdarg.h>
 /**
- * char_printing - prints a character
- * @args: argument list
- * Return: 1 (number of characters printed).
- */
-
+* char_printing - prints a character
+* @args: argument list
+* Return: 1 (number of characters printed).
+*/
 int char_printing(va_list args)
 {
-    int c = va_arg(args, int);
-    int result = (write(1, &c, 1));
-   
-    if (result == -1)
-        return (-1);
-    else
-        return (result);
-    
+int c = va_arg(args, int);
+int result = (write(1, &c, 1));
+if (result == -1)
+return (-1);
+else
+return (result);
 }
 
